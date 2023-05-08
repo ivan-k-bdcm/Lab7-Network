786ee60d2c56fdd72c024e4437a89e5ee0589a6b
Ivan <kuriloivan2003@gmail.com>
2023-05-08 19:35:21 +0300

    Greating

---
+++ API.py
@@ -0,0 +1,26 @@
+import requests
+
+# Вітаємо користувача і виводимо список категорій
+print("Вітаємо! Оберіть категорію зі списку:")
+print("animal")
+
+# Отримуємо категорію від користувача
+category = input()
+
+# Формуємо URL для API з використанням введеної категорії
+api_url = f"https://api.chucknorris.io/jokes/random?category={category}"
+
+# Відправляємо запит до API
+response = requests.get(api_url)
+
+# Перевіряємо, чи є успішна відповідь
+if response.status_code == 200:
+    # Парсимо JSON-відповідь
+    json_data = response.json()
+
+    # Виводимо дані в консоль
+    print("\nКатегорія:", json_data["categories"][0])
+    print("Дата створення:", json_data["created_at"])
+    print("Анекдот:", json_data["value"])
+else:
+    print("Сталася помилка при отриманні даних з API")
